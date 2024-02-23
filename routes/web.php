bca800b77959c93a30f1ffc2f8d17a81cccd7137
Tan <tan.a25@student.passerellesnumeriques.org>
2024-02-23 10:00:41 +0700

    sua bai tap 4

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,61 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $html='<h1>Học lap trinh tai unicode</h1>';
+    return $html;
 });
 
-Route::get('/unicode', function () {
-    return view('home');
+Route::get('unicode', function () {
+    return view ('form');
 });
+Route::post('unicode', function () {
+    $html='<h1>Phuong thuc post cua path /unicode</h1>';
+    return $html;
+});
+Route::put('unicode', function () {
+    $html='<h1>Phuong thuc put cua path /unicode</h1>';
+    return $html;
+});
+Route::delete('unicode', function () {
+    $html='<h1>Phuong thuc delete cua path /unicode</h1>';
+    return $html;
+});
+// Route::patch('unicode', function () {
+//     $html='<h1>Phuong thuc patch cua path /unicode</h1>';
+//     return $html;
+// });
+// Route::match(['get','post'],'unicode', function () {
+//     return $_SERVER['REQUEST_METHOD'];
+// });
+// // route::any('unicode',function(){
+// //     // return $_SERVER['REQUEST_METHOD'];
+// //     return $request->method();
+// // });
+// route::get ('show-form',function(){
+//     return view ('form');
+// });
+// route::redirec('unicode','show-form');
+// route::view('show-fform','form');
+// route::prefix('admin')->group(function(){
+//     Route::get('unicode', function () {
+//         return view ('form');
+//     });
+//     route::get ('show-form',function(){
+//         return view ('form');
+//     });
+//     route::prefix('product')->group(function(){
+//         Route::get('/', function () {
+//             return 'danh sach san pham';
+//         });
+//         route::get ('add',function(){
+//             return 'them san pham';
+//         });
+//         route::get ('edit',function(){
+//             return 'sua san pham';
+//         });
+//         route::get ('delete',function(){
+//             return 'xoa san pham';
+//         });
+//     });
+// });
 
-Route::get('/san-pham', function () {
-    return view('product');
-});
