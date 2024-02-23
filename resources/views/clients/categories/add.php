88ace6f071ebe4dc436f50adb5a62f648ecfa523
Tan <tan.a25@student.passerellesnumeriques.org>
2024-02-24 00:31:51 +0700

    sua bai 6

---
+++ resources/views/clients/categories/add.php
@@ -0,0 +1,8 @@
+<h1>them chuyen muc</h1>
+<form method="post" action="<?php echo route('categories.add')  ?>">
+    <div>
+        <input type= "text" name ="category_name" placeholder="ten chuyen muc">
+    </div>
+    <button type="Submit">them chuyen muc</button>
+    <input type= "hidden" name ="_token" value="<?php echo csrf_token(); ?>">
+</form>
