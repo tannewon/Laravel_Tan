5033ccd6a2ce8c082f0b96e40961ec7e3c67ec90
Tan <tan.a25@student.passerellesnumeriques.org>
2024-02-26 22:21:38 +0700

    sua bai 9

--- resources/views/clients/categories/products/Detail.blade.php
+++ resources/views/clients/categories/products/Detail.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>chi tiet san pham{{!empty()}} </title>
+    <title>chi tiet san pham </title>
 </head>
 <body>
-    
+    <h1>chi tiet san pham{{!empty($name)?$name:false}}</h1>
 </body>
 </html>
